5bebda513fc1cac2349848e9aec312e4a7b92c3d
ext_thk <tharuka.kannangara95@gmail.com>
2026-03-24 16:36:01 -0400

    update nodejs lambda function

--- terraform-infra/lambda_api.tf
+++ terraform-infra/lambda_api.tf
@@ -1,20 +1,27 @@
-resource "aws_lambda_function" "dynamodb_read" {
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  # Point this to the folder containing your index.mjs
+  source_dir  = "${path.module}/lambda/dynamodb_api/" 
+  
+  # Output the zip to the root of your terraform module
+  output_path = "${path.module}/lambda/dynamodb_api/dynamodb_read_payload.zip" 
+}
 
+resource "aws_lambda_function" "dynamodb_read" {
   function_name = "dynamodb-read"
   runtime       = "nodejs20.x"
-  handler       = "lambda.handler"
+  handler       = "index.handler" 
+  role          = aws_iam_role.lambda_role.arn
 
-  role = aws_iam_role.lambda_role.arn
-
-  filename         = "${path.module}/lambda/dynamodb_api/dynamodb_read.zip"
-  source_code_hash = filebase64sha256("${path.module}/lambda/dynamodb_api/dynamodb_read.zip")
+  # Point these directly to the output of the archive_file data source
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   memory_size = 128
   timeout     = 3
 }
 
 resource "aws_iam_role_policy" "lambda_dynamodb_read" {
-
   role = aws_iam_role.lambda_role.id
 
   policy = jsonencode({
